2a466e1d15df2c224bc59acb0e606d28f98e35fc
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-08-01 18:10:40 +0200

    feat(user): update userv  entity

--- src/main/java/com/MagicTheGathering/user/User.java
+++ src/main/java/com/MagicTheGathering/user/User.java
@@ -1,4 +1,36 @@
 package com.MagicTheGathering.user;
 
+import com.MagicTheGathering.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
 }
