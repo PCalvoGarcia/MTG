3748e37d01a929b00b8941d850cc50d995f128d7
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-08-05 09:17:54 +0200

    feat(userService): base service

---
+++ src/main/java/com/MagicTheGathering/user/UserService.java
@@ -0,0 +1,11 @@
+package com.MagicTheGathering.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+}
