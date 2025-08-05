0e6a054b50de38ddc74ace7bd44bc5311e8694d5
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-08-05 17:53:07 +0200

    feat: add enums for card entity

---
+++ src/main/java/com/MagicTheGathering/legality/legality.java
@@ -0,0 +1,13 @@
+package com.MagicTheGathering.legality;
+
+public enum legality {
+    STANDARD,
+    MODERN,
+    COMMANDER,
+    PIONEER,
+    LEGACY_VINTAGE,
+    PAUPER,
+    BRAWL,
+    BOOSTER_DRAFT,
+    SEALED_DECK
+}
---
+++ src/main/java/com/MagicTheGathering/manaColor/manaColor.java
@@ -0,0 +1,10 @@
+package com.MagicTheGathering.manaColor;
+
+public enum manaColor {
+    WHITE,
+    BLUE,
+    BLACK,
+    RED,
+    GREEN,
+    COLORLESS
+}
